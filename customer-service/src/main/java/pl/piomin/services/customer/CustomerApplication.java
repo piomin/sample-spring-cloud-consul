daf81c75663d510c0db56972e467464489aacad2
piomin <piotr.minkowski@gmail.com>
2023-06-18 00:25:06 +0200

    update to java 20

--- customer-service/src/main/java/pl/piomin/services/customer/CustomerApplication.java
+++ customer-service/src/main/java/pl/piomin/services/customer/CustomerApplication.java
@@ -13,28 +13,28 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 @SpringBootApplication
 @EnableFeignClients
 public class CustomerApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-	}
 
-	@Bean
-	public CommonsRequestLoggingFilter requestLoggingFilter() {
-	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-	    loggingFilter.setIncludePayload(true);
-	    loggingFilter.setIncludeHeaders(true);
-	    loggingFilter.setMaxPayloadLength(1000);
-	    loggingFilter.setAfterMessagePrefix("REQ:");
-	    return loggingFilter;
-	}
-	
-	@Bean
-	CustomerRepository repository() {
-		CustomerRepository repository = new CustomerRepository();
-		repository.add(new Customer("John Scott", CustomerType.NEW));
-		repository.add(new Customer("Adam Smith", CustomerType.REGULAR));
-		repository.add(new Customer("Jacob Ryan", CustomerType.VIP));
-		return repository;
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setMaxPayloadLength(1000);
+        loggingFilter.setAfterMessagePrefix("REQ:");
+        return loggingFilter;
+    }
+
+    @Bean
+    CustomerRepository repository() {
+        CustomerRepository repository = new CustomerRepository();
+        repository.add(new Customer("John Scott", CustomerType.NEW));
+        repository.add(new Customer("Adam Smith", CustomerType.REGULAR));
+        repository.add(new Customer("Jacob Ryan", CustomerType.VIP));
+        return repository;
+    }
+
 }
